2c4ba43d95e918161e43bdd30669e704ad037de5
Christian Thomas <76794573+christiancthomas@users.noreply.github.com>
2022-12-25 17:32:28 -0600

    added initial support for creating events/Users/christianthomas/Developer/google-smart-calendar/venv/bin/python

--- src/services/create_event.py
+++ src/services/create_event.py
@@ -4,37 +4,40 @@ from src.services.auth import auth
 
 def create():
     event = {
-    'summary': 'Google I/O 2022',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-        'dateTime': '2022-12-25T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'end': {
-        'dateTime': '2022-12-25T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-    ],
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10},
-        ],
-    },
+    # 'summary': 'Google I/O 2022',
+    # 'location': '800 Howard St., San Francisco, CA 94103',
+    # 'description': 'A chance to hear more about Google\'s developer products.',
+    # 'start': {
+    #     'dateTime': '2022-12-25T09:00:00-07:00',
+    #     'timeZone': 'America/Los_Angeles',
+    # },
+    # 'end': {
+    #     'dateTime': '2022-12-25T17:00:00-07:00',
+    #     'timeZone': 'America/Los_Angeles',
+    # },
+    # 'recurrence': [
+    #     'RRULE:FREQ=DAILY;COUNT=2'
+    # ],
+    # 'attendees': [
+    #     {'email': 'lpage@example.com'},
+    #     {'email': 'sbrin@example.com'},
+    # ],
+    # 'reminders': {
+    #     'useDefault': False,
+    #     'overrides': [
+    #     {'method': 'email', 'minutes': 24 * 60},
+    #     {'method': 'popup', 'minutes': 10},
+    #     ],
+    # },
     }
 
     print('\nstarting create()...\n')
     try:
         creds = auth()
         service = build('calendar', 'v3', credentials=creds)
+        event['summary'] = input('Event name: ')
+        print(event['summary'])
+
         event = service.events().insert(calendarId='primary', body=event).execute()
         print('Event created: %s' % (event.get('htmlLink')))
 
